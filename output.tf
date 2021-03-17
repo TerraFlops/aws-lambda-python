9eee7bf7d7a2b32acd651f5184a983233ebb5616
Damian Sloane <damian@capacitorlabs.com.au>
2021-03-17 11:18:18 +1100

    Work in progress

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "lambda_function_arn" {
   value = aws_lambda_function.lambda.arn
 }
 
+output "lambda_function_function_name" {
+  value = aws_lambda_function.lambda.function_name
+}
+
 output "lambda_function_version" {
   value = aws_lambda_function.lambda.version
 }
